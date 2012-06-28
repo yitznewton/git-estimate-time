12f306b5b04785b14d083971fc7c7a4055595324
Yitzchak Schaffer <yitzchak.schaffer@vitals.com>
2012-06-28 14:24:24 -0400

    use shell 'git log' command instead of GitPython

--- git-estimate-time.py
+++ git-estimate-time.py
@@ -1,23 +1,33 @@
 #!/usr/bin/python
 
-from git import *
-from os import getcwd
+from os import getcwd, popen
 import sys
 import re
+from pprint import pprint
 
-def get_relevant_entries(log, start_hex, end_hex):
+def get_relevant_entries(start_hex, end_hex):
     should_use = False
     entries = []
 
-    for item in log:
-        current_hex = item[1]
-        if current_hex == start_hex:
+    command = 'git log --reverse --format=format:"%H\t%ae\t%at"'
+    git_log_output = popen(command)
+
+    while 1:
+        line = git_log_output.readline()
+
+        if not line:
+            break
+
+        pattern = re.compile("^([0-9a-f]+)\t([^\t]+)\t([\d]+)$")
+        matches = pattern.match(line.strip())
+        
+        if matches.group(1) == start_hex:
             should_use = True
 
         if should_use:
-            entries.append(item)
+            entries.append(matches.groups())
 
-        if current_hex == end_hex:
+        if matches.group(1) == end_hex:
             break
 
     return entries
@@ -32,7 +42,7 @@ def get_work_time(entries, seconds_before_commit):
                 total_times[user] = seconds_before_commit
                 continue
 
-            since_last = times[i] - times[i-1]
+            since_last = int(times[i]) - int(times[i-1])
 
             if since_last < seconds_before_commit:
                 total_times[user] += since_last
@@ -45,8 +55,8 @@ def get_user_commit_times(entries, seconds_before_commit):
     commits_by_user = {}
 
     for item in entries:
-        name = item[2].name
-        time = item[3][0] + item[3][1]
+        name = item[1]
+        time = item[2]
 
         if name not in commits_by_user:
             commits_by_user[name] = []
@@ -63,12 +73,7 @@ assert re.compile("\d+").match(sys.argv[3])
 
 seconds_before_commit = int(sys.argv[3])
 
-repo = Repo(getcwd())
-assert repo.bare == False
-
-head = repo.head.reference
-
-relevant_log_entries = get_relevant_entries(head.log(), sys.argv[1], sys.argv[2])
+relevant_log_entries = get_relevant_entries(sys.argv[1], sys.argv[2])
 
 assert len(relevant_log_entries) > 0
 
